790b62fe0c3806c143f35449ee81203fee9be990
catatonicprime <catatonicprime@gmail.com>
2019-11-03 21:03:12 -0700

    Looks like it is working.

--- test.py
+++ test.py
@@ -24,11 +24,11 @@ class Node:
     [ServerName][ server][\n] -> children / pretokens
     [</VirtualHost][>][\n] -> posttokens
     """
-    def __init__(self, pretokens, posttokens, children, parent=None):
+    def __init__(self, pretokens, children, posttokens, parent=None):
         self._parent = parent
         self._pretokens = pretokens
-        self._posttokens = posttokens
         self._children = children
+        self._posttokens = posttokens
 
     @property
     def tokens(self):
@@ -39,7 +39,8 @@ class Node:
         for token in self._pretokens:
             tokenList.append(token)
         for child in self._children:
-            tokenList.append(child.tokens)
+            for token in child.tokens:
+                tokenList.append(token)
         for token in self._posttokens:
             tokenList.append(token)
         return tokenList
@@ -70,8 +71,6 @@ class Node:
         if len(self.tokens) == 0:
             return s
         for token in self.tokens:
-            if len(token) == 0:
-                continue
             s += "{}".format(token[1])
         return s
 
@@ -92,21 +91,39 @@ class ConfigFile:
 
     def ParseNode(self, parent=None):
         node = Node([], [], [], parent=parent)
-        currentList = node.pretokens
+        # Flag that indicates we will be exiting a scoped directive after this node completes building.
+        closeTag = False
         for token in self._stream:
-            currentList.append(token)
+            if token[0] is Token.Error:
+                raise ValueError("Config has errors, bailing.")
+            node.pretokens.append(token)
+            # Nodes don't have types until their first non-whitespace token is matched.
             if not node.type:
-                # Occurs when the only tokens so far have been whitespace. Append and move on.
                 continue
+            # The node has a type, the lexer will return either a Token.Text with an empty OR newline string before
+            # the next node info is available.
             if token[0] is Token.Text and token[1] == '':
                 return node
             if token[0] is Token.Text and token[1] == '\n':
                 return node
-            if token[0] is Token.Name.Tag and token[1][0] == '>' and currentList == node.pretokens:
-                child = self.ParseNode()
-                node.children.append(child)
-                currentList = node.posttokens
-            elif token[0] is Token.Name.Tag and token[1][0] == '>' and currentList == node.posttokens:
+            # When handling Tag tokens, e.g. nested components, we need to know if we're at the start OR end of the Tag.
+            # Check for </ first and flag that this node will be the closing tag or not, if so and > is encountered
+            # then return this node since it is complete.
+            if token[0] is Token.Name.Tag and token[1][0] == '<' and token[1][1] == '/':
+                closeTag = True
+            if token[0] is Token.Name.Tag and token[1][0] == '>' and closeTag:
+                return node
+            # We're starting a Tag instead, begin building out the children nodes for this node.
+            elif token[0] is Token.Name.Tag and token[1][0] == '>':
+                child = self.ParseNode(parent=node)
+                while child:
+                    node.children.append(child)
+                    child = self.ParseNode(parent=node)
+                # The last node should always be the </tag>, so we'll pop it off & migrate it's tokens into this nodes
+                # posttokens.
+                closeNode = node.children.pop()
+                for pt in closeNode.tokens:
+                    node.posttokens.append(pt)
                 return node
         return None
 
@@ -119,6 +136,6 @@ class ConfigFile:
         return s
 
 # Get a stream of tokens!
-cf = ConfigFile(file="httpd_short.conf")
+cf = ConfigFile(file="httpd.conf")
 print(cf)
 
