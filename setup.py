83d755bbafbe73aacab3d44eb142bda1b18c0b13
catatonicprime <catatonicprime@gmail.com>
2019-11-04 17:39:33 -0700

    aligning toward a package for future public use

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='sacp',
+        version='0.1',
+        scripts=[''],
+        install_requires=[
+            'pygments'
+        ],
+        author='catatonicprime',
+        author_email='catatonicprime@gmail.com',
+        description='A simple Apache Config Parser',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/catatonicprime/sacp',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            ],
+        )
