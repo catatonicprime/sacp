4c7e51424c720460b73b90d40ef5ce810d8284b6
catatonicprime <catatonicprime@gmail.com>
2019-11-27 14:58:27 -0700

    Removing ported ApacheConfLexer in favor of distributed version (available with pygments 2.5.0+)

--- sacp/base.py
+++ sacp/base.py
@@ -1,56 +1,7 @@
-from pygments.lexer import RegexLexer, default, words, bygroups, include, using
-from pygments.token import Text, Comment, Operator, Keyword, Name, String, \
-    Number, Punctuation, Whitespace, Literal
-from pygments.lexers.shell import BashLexer
-from pygments.lexers.data import JsonLexer
-
 from .node import *
+from pygments.lexers.configs import ApacheConfLexer
 import pygments
 import glob
-
-
-# Stolen from pygments for the purposes of fixing...
-# TODO: remove this class when pygments 2.5.0 is released.
-class ApacheConfLexer(RegexLexer):
-    """
-    Lexer for configuration files following the Apache config file
-    format.
-
-    .. versionadded:: 0.6
-    """
-
-    name = 'ApacheConf'
-    aliases = ['apacheconf', 'aconf', 'apache']
-    filenames = ['.htaccess', 'apache.conf', 'apache2.conf']
-    mimetypes = ['text/x-apacheconf']
-    flags = re.MULTILINE | re.IGNORECASE
-
-    tokens = {
-        'root': [
-            (r'\s+', Text),
-            (r'#(.*\\\n)+.*$|(#.*?)$', Comment),
-            (r'(<[^\s>]+)(?:(\s+)(.*))?(>)',
-             bygroups(Name.Tag, Text, String, Name.Tag)),
-            (r'([a-z]\w*)(\s+)',
-             bygroups(Name.Builtin, Text), 'value'),
-            (r'\.+', Text),
-        ],
-        'value': [
-            (r'\\\n', Text),
-            (r'$', Text, '#pop'),
-            (r'\\', Text),
-            (r'[^\S\n]+', Text),
-            (r'\d+\.\d+\.\d+\.\d+(?:/\d+)?', Number),
-            (r'\d+', Number),
-            (r'/([a-z0-9][\w./-]+)', String.Other),
-            (r'(on|off|none|any|all|double|email|dns|min|minimal|'
-             r'os|productonly|full|emerg|alert|crit|error|warn|'
-             r'notice|info|debug|registry|script|inetd|standalone|'
-             r'user|group)\b', Keyword),
-            (r'"([^"\\]*(?:\\(.|[\n])[^"\\]*)*)"', String.Double),
-            (r'[^\s"\\]+', Text)
-        ],
-    }
 
 
 class Parser:
