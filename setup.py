bd14b3b23aabbaccc2fe6dc485b4447a165bd432
catatonicprime <catatonicprime@gmail.com>
2019-12-12 16:53:07 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name='sacp',
-        version='0.3',
+        version='0.4',
         install_requires=[
             'pygments'
         ],
